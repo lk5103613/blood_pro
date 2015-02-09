09de38acb357cce14c6fbb162fff95addee215ef
joselyncui <15895026586@163.com>
2015-02-09 18:39:57 +0800

    add comments

--- BloodPro_4_0/src/com/wm/tools/Uuids.java
+++ BloodPro_4_0/src/com/wm/tools/Uuids.java
@@ -3,23 +3,23 @@ package com.wm.tools;
 public class Uuids {
 	
 	public static String service1 = "00001800-0000-1000-8000-00805f9b34fb";
-	public static String characteristic1_1 = "00002a00-0000-1000-8000-00805f9b34fb";
-	public static String characteristic1_2 = "00002a01-0000-1000-8000-00805f9b34fb";
-	public static String characteristic1_3 = "00002a02-0000-1000-8000-00805f9b34fb";
+	public static String characteristic1_1 = "00002a00-0000-1000-8000-00805f9b34fb";//device name
+	public static String characteristic1_2 = "00002a01-0000-1000-8000-00805f9b34fb";//appearance
+	public static String characteristic1_3 = "00002a02-0000-1000-8000-00805f9b34fb";//周边隐私标志
 //	public static String characteristic1_4 = "00002a03-0000-1000-8000-00805f9b34fb";
-	public static String characteristic1_5 = "00002a04-0000-1000-8000-00805f9b34fb";
-	public static String service2 = "00001801-0000-1000-8000-00805f9b34fb";
+	public static String characteristic1_5 = "00002a04-0000-1000-8000-00805f9b34fb";//外围首选连接参数
+	public static String service2 = "00001801-0000-1000-8000-00805f9b34fb";//Generic Attribute
 //	public static String characteristic2_1 = "00002a05-0000-1000-8000-00805f9b34fb";
-	public static String services3 = "0000180a-0000-1000-8000-00805f9b34fb";
+	public static String services3 = "0000180a-0000-1000-8000-00805f9b34fb";//Device Information
 	public static String characteristic3_1 = "00002a23-0000-1000-8000-00805f9b34fb";
-	public static String characteristic3_2 = "00002a24-0000-1000-8000-00805f9b34fb";
+	public static String characteristic3_2 = "00002a24-0000-1000-8000-00805f9b34fb";//型号字符串
 	public static String characteristic3_3 = "00002a25-0000-1000-8000-00805f9b34fb";
-	public static String characteristic3_4 = "00002a26-0000-1000-8000-00805f9b34fb";
-	public static String characteristic3_5 = "00002a27-0000-1000-8000-00805f9b34fb";
+	public static String characteristic3_4 = "00002a26-0000-1000-8000-00805f9b34fb";//固件版本字符串
+	public static String characteristic3_5 = "00002a27-0000-1000-8000-00805f9b34fb";//硬件版本字符串
 	public static String characteristic3_6 = "00002a28-0000-1000-8000-00805f9b34fb";
-	public static String characteristic3_7 = "00002a29-0000-1000-8000-00805f9b34fb";
-	public static String characteristic3_8 = "00002a2a-0000-1000-8000-00805f9b34fb";
-	public static String characteristic3_9 = "00002a50-0000-1000-8000-00805f9b34fb";
+	public static String characteristic3_7 = "00002a29-0000-1000-8000-00805f9b34fb";//制造商名称字符串
+	public static String characteristic3_8 = "00002a2a-0000-1000-8000-00805f9b34fb";//IEEE11073-20601监管认证数据列表
+	public static String characteristic3_9 = "00002a50-0000-1000-8000-00805f9b34fb";//PnP ID
 	public static String services4 = "0000fff0-0000-1000-8000-00805f9b34fb";
 	public static String characteristic4_1 = "0000fff1-0000-1000-8000-00805f9b34fb";
 	public static String characteristic4_2 = "0000fff2-0000-1000-8000-00805f9b34fb";
